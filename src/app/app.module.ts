47f685d2b5b91677d551bb7fa8d490bb5980ef3c
julietaArgollas <julieta.c.argollas@gmail.com>
2021-06-27 23:14:12 -0400

    gerencia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopnavComponent } from './components/topnav/topnav.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CopyrightComponent } from './components/copyright/copyright.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopnavComponent,
+    NavbarComponent,
+    CopyrightComponent
   ],
   imports: [
     BrowserModule,
